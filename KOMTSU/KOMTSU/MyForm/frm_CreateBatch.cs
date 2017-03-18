0732477200f6951f9dba21ab74c8bb375d5613cf
Nguyen Tien Hoang <nguyentienhoang139@gmail.com>
2017-03-18 11:32:44 +0700

    Create Project

---
+++ KOMTSU/KOMTSU/MyForm/frm_CreateBatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace KOMTSU.MyForm
+{
+    public partial class frm_CreateBatch : DevExpress.XtraEditors.XtraForm
+    {
+        public frm_CreateBatch()
+        {
+            InitializeComponent();
+        }
+    }
+}
